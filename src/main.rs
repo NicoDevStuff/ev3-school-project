b02b65ae2c38c5a1132123357768ea92b5717a01
NicoDevStuff <NicoG2008@gmx.de>
2023-04-27 16:40:03 +0200

    🤓🤓🤓🤓🤓🤓🤓🤓🤓🤓🤓

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 extern crate ev3dev_lang_rust;
 
-use ev3dev_lang_rust::motors::{MediumMotor, MotorPort};
+use ev3dev_lang_rust::motors::{MediumMotor, MotorPort, LargeMotor};
 use ev3dev_lang_rust::sensors::UltrasonicSensor;
 use ev3dev_lang_rust::sound;
 use ev3dev_lang_rust::Ev3Result;
@@ -14,17 +14,27 @@ const FOV: f32 = 70f32;
 const SPEED: i32 = 30i32;
 
 fn main() -> Ev3Result<()> {
-    let mut engine = console_engine::ConsoleEngine::init(44, 21, 30).unwrap();
+    let w = 134f32;
+
+    let h = 30f32;
+
+    let mut engine = console_engine::ConsoleEngine::init(w as u32, h as u32, 10).unwrap();
 
     let ultrasonicsensor = UltrasonicSensor::find()?;
 
     let radar_motor = MediumMotor::get(MotorPort::OutB)?;
 
-    sound::speak("öööufuihfeufewhufewhjfewfewhufewfiuhw")?.wait()?;
+    let l_motor = LargeMotor::get(MotorPort::OutA)?;
+    let r_motor = LargeMotor::get(MotorPort::OutD)?;
+    //sound::speak("öööufuihfeufewhufewhjfewfewhufewfiuhw")?.wait()?;
     
     radar_motor.reset()?;
 
     radar_motor.run_direct()?;
+
+    r_motor.run_direct()?;
+
+    l_motor.run_direct()?;
 
     radar_motor.set_duty_cycle_sp(SPEED)?;
 
@@ -34,28 +44,60 @@ fn main() -> Ev3Result<()> {
 
     let mut prevx: i32 = 0;
 
+    let mut steer_r: f32 = 0.;
+    let mut steer_l: f32 = 0.;
+    
     loop {
+        steer_r = 0f32;
+        steer_l = 0f32;
         distance = ultrasonicsensor.get_distance().unwrap();
         engine.wait_frame();
 
         let position: f32 = radar_motor.get_position()? as f32;
 
-        height = 21f32 * (1f32 - distance as f32 / 255f32);
+        height = h * (1f32-(distance.clamp(0, 1000) as f32 / 1000f32));
 
-        x = ((position / FOV / 2f32 + 0.5f32) * 44f32) as i32;
+        x = ((position / FOV / 2f32 + 0.5f32) * w) as i32;
 
         if engine.is_key_pressed(KeyCode::Char('q')) {
             radar_motor.set_duty_cycle_sp(0)?;
             break;
         }
-        engine.fill_rect(prevx, 0, x, 21, pixel::pxl_fg(' ', Color::White));
+
+        if engine.is_key_held(KeyCode::Up) {
+             steer_r += 1f32;
+             steer_l += 1f32; 
+        }
+        if engine.is_key_held(KeyCode::Right) {
+            steer_l += 1f32;
+            steer_r = 0f32;
+        }
+
+        if engine.is_key_held(KeyCode::Left) {
+            steer_r += 1f32;
+            steer_l = 0f32;
+        }
+
+        if engine.is_key_held(KeyCode::Down) {
+             steer_r -= 1f32;
+             steer_l -= 1f32; 
+        }
+
+
+
+        r_motor.set_duty_cycle_sp((steer_r*100f32) as i32);
+        l_motor.set_duty_cycle_sp((steer_l*100f32) as i32);
+
+        engine.fill_rect(prevx, 0, x, h as i32, pixel::pxl_fg(' ', Color::White));
         engine.fill_rect(
             prevx,
-            ((21f32 - height) / 2f32) as i32,
+            ((h - height) / 2f32) as i32,
             x,
-            (21f32 - ((21f32 - height) / 2f32)) as i32,
-            pixel::pxl_fg('█', Color::Black),
+            (h - ((h - height) / 2f32)) as i32,
+            pixel::pxl_fg('█', Color::White),
         );
+        
+        engine.print(0, 0, &distance.to_string());
 
         prevx = x;
 
